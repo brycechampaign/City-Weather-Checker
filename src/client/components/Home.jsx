8d9c7e5aeb4fda1dcab3b1171d1abe2bda35a150
Bryce Champaign <bechampaign@hotmail.com>
2020-09-25 23:30:27 -0400

    Set up basic front-end with placeholder text

---
+++ src/client/components/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <h1>Placeholder</h1>;
+};
+
+export default Home;
---
+++ src/client/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Home from './components/Home.jsx';
+
+render(<Home />, document.getElementById('root'));
