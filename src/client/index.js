64f9e3c87d821ceeedd7b68ae7b45aaed7cbb611
Bryce Champaign <bechampaign@hotmail.com>
2020-09-26 23:32:07 -0400

    Import core-js and regenerator-runtime
    These packages are needed for Babel to handle asynchronous function calls.

--- src/client/index.js
+++ src/client/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import Home from './components/Home.jsx';
 
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 render(<Home />, document.getElementById('root'));
