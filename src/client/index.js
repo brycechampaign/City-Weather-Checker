2dcd79fb17c86735319bea3f84f304fe85ef43f8
Bryce Champaign <bechampaign@hotmail.com>
2020-09-28 21:43:02 -0400

    Render App instead of Home
    The App component now contains the Home component. As the highest level component, it should be rendered by Index.js.

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Home from './components/Home.jsx';
+import App from './components/App.jsx';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-render(<Home />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
